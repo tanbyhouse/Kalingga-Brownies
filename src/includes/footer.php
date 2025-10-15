8ee5720c081a7dbc66e33117b1509f860739769f
Tan <aliyaliyaa00@gmail.com>
2025-10-15 21:46:34 +0700

    update footer color

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,4 +1,4 @@
-</main> <footer class="text-center p-4 mt-auto bg-gray-800 text-white">
+</main> <footer class="text-center p-4 mt-auto bg-red-950 bg-opacity-80 text-white">
         <p>&copy; <?= date('Y'); ?> Kalingga Brownies. All Rights Reserved.</p>
     </footer>
 </div> <script src="js/app.js"></script>

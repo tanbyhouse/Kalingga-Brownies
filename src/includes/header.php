cb44a17fd67e13b50df91fd670b3765e0242f260
Tan <aliyaliyaa00@gmail.com>
2025-10-15 20:43:15 +0700

    switch to tailwind cdn

--- src/includes/header.php
+++ src/includes/header.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kalingga Brownies</title>
-    <link href="css/output.css" rel="stylesheet">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body class="bg-gray-100 font-sans">
-    <main class="container mx-auto p-4">
-
-    
+    <div class="flex flex-col min-h-screen">
+        <main class="flex-grow">
